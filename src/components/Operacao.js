39c8fa60651fed96f3a5bc5a73d414379ea98a37
guilhermeferrari <guilhermefbravin@gmail.com>
2018-09-13 18:41:13 -0300

    alteracoes em Operacao.js para funcionamento do picker
    remove commit

--- src/components/Operacao.js
+++ src/components/Operacao.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Text, View, StyleSheet, TextInput, Picker } from 'react-native';
 
-export default props => (
-    <View>
-        <Picker>
-            <Picker.Item label='Soma' value='soma' />
-            <Picker.Item label='Subtração' value='subtracao' />
-        </Picker>
-    </View>
-)
+export default class Operacao extends Component {
+    constructor(props){
+        super(props)
+        this.state= {
+            operacao: ''
+        }
+    }
+    render() {
+        return (
+            <View>
+                <Picker selectedValue={this.state.operacao} onValueChange={op =>{
+                    this.setState({operacao:op})
+                }}>
+                    <Picker.Item label='Soma' value='soma' />
+                    <Picker.Item label='Subtração' value='subtracao' />
+                </Picker>
+            </View>
+        )
+    }
+}
 
 const styles = StyleSheet.create({
     selec: {
