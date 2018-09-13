48230a8c627a854b8c50049a326313cde70910d4
guilhermeferrari <guilhermefbravin@gmail.com>
2018-09-13 20:10:29 -0300

    ajustes para obter a operaçao selecionada em Painel.js

--- src/components/Operacao.js
+++ src/components/Operacao.js
@@ -2,17 +2,11 @@ import React, { Component } from 'react'
 import { Text, View, StyleSheet, TextInput, Picker } from 'react-native';
 
 export default class Operacao extends Component {
-    constructor(props){
-        super(props)
-        this.state= {
-            operacao: ''
-        }
-    }
     render() {
         return (
             <View>
-                <Picker selectedValue={this.state.operacao} onValueChange={op =>{
-                    this.setState({operacao:op})
+                <Picker selectedValue={this.props.operacao} onValueChange={op =>{
+                    this.props.mudaOperacao(op)
                 }}>
                     <Picker.Item label='Soma' value='soma' />
                     <Picker.Item label='Subtração' value='subtracao' />
